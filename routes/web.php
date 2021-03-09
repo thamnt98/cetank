d5188991b8d2f49bf1bed2b846397d7fcb1caaa6
thamnt98 <tham27011@gmail.com>
2021-03-09 22:52:44 +0700

    refactor code

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,19 @@ Route::get('admin/login', 'Admin\LoginController@showLoginForm')->name('admin.lo
 Route::post('admin/login', 'Admin\LoginController@login')->name('admin.login.post');
 Route::get('admin-dashboard', 'DashboardController@getDashboard')->name('dashboard');
 Route::get('/menu/{id}/{name}', 'HomeController@getMenu');
+Route::get('category/{slug}', 'HomeController@getList')->name('post.list');
+Route::get('blog-details/{slug}','HomeController@detailsBlog')->name('blog.detail');
 Auth::routes();
 
 Route::group(['prefix' => 'admin'], function () {
-
     Route::group(['prefix' => 'post'], function () {
         Route::get('all', 'PostController@index')->name('post.all');
-        Route::get('/{slug}', 'HomeController@getList')->name('post.list');
         Route::get('edit/{id}', 'PostController@edit')->name('post.edit');
         Route::post('update', 'PostController@update')->name('post.update');
         Route::post('delete', 'PostController@destroy')->name('post.delete');
         Route::post('publish', 'PostController@publish')->name('post.publish');
         Route::get('create', 'PostController@create')->name('post.create');
         Route::post('store', 'PostController@store')->name('post.store');
-        Route::get('blog-details/{slug}','HomeController@detailsBlog')->name('blog.detail');
     });
 
     Route::group(['prefix' => 'menu'], function () {
