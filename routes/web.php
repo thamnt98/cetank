6edd020186a164611a998ad6cbf93c4588e09654
thamnt98 <tham27011@gmail.com>
2021-03-14 23:13:19 +0700

    add footer blog

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', 'HomeController@getIndex')->name('home');
 Route::get('admin/login', 'Admin\LoginController@showLoginForm')->name('admin.login');
 Route::post('admin/login', 'Admin\LoginController@login')->name('admin.login.post');
 Route::get('admin-dashboard', 'DashboardController@getDashboard')->name('dashboard');
-Route::get('/{slug}', 'HomeController@getList')->name('post.list')->where('slug', '(.*)');
-Route::get('tags/{tag}', 'HomeController@getPostByTag')->name('tag.list')->where('tag', '(.*)');
-Route::get('post/{slug}','HomeController@detailsBlog')->name('post.detail')->where('slug', '(.*)');
+Route::get('/{slug}', 'HomeController@getList')->name('post.list');
+Route::get('tags/{tag}', 'HomeController@getPostByTag')->name('tag.list');
+Route::get('post/{slug}','HomeController@detailsBlog')->name('post.detail');
 Auth::routes();
 
 Route::group(['prefix' => 'admin'], function () {
