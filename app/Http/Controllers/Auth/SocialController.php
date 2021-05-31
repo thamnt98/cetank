eff3a93766358375c0cbe4507440be75387991b7
Nguyen Thi Tham <thamnt@newit.co.jp>
2021-05-31 21:53:40 +0700

    fix bug login by facebook

--- app/Http/Controllers/Auth/SocialController.php
+++ app/Http/Controllers/Auth/SocialController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use http\Env\Url;
-use Intervention\Image\Facades\Image;
 use Socialite;
-use File;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class SocialController extends Controller
 {
@@ -18,25 +18,23 @@ class SocialController extends Controller
     public function callback($provider)
     {
         $getInfo = Socialite::driver($provider)->user();
-        $user = $this->createUser($getInfo,$provider);
-        auth()->login($user);
-        return redirect()->route('home');
-    }
-    function createUser($getInfo,$provider){
         $user = User::where('provider_id', $getInfo->id)->first();
-        $image = $getInfo->avatar ."&access_token={$getInfo->token}";
-        $fileContents = file_get_contents($image);
-        $img = env('APP_URL') . 'images/'. $getInfo->id . ".jpg";
-        $image = file_put_contents($img, $fileContents);
-        if (!$user) {
+        if (is_null($user)) {
+
+            $image = $getInfo->avatar . "&access_token={$getInfo->token}";
+            $fileContents = file_get_contents($image);
+            $img = env('APP_URL') . 'images/' . $getInfo->id . ".jpg";
+            $image = file_put_contents($img, $fileContents);
             $user = User::create([
                 'name'     => $getInfo->name,
                 'email'    => $getInfo->email,
                 'provider' => $provider,
                 'provider_id' => $getInfo->id,
-                'image' =>  $getInfo->id . ".jpg"
+                'image' =>  $getInfo->id . ".jpg",
+                'password' => Hash::make(Str::random(8))
             ]);
         }
-        return $user;
+        Auth::login($user);
+        return redirect()->route('home');
     }
 }
